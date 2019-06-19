7920d6fe14c554a482d250af5d405d06f60cc0f4
bkushigian <bkushigian@gmail.com>
2019-06-19 14:02:38 -0400

    Started Syntax.hs

--- toy-lambdas/src/Inductive/Syntax.hs
+++ toy-lambdas/src/Inductive/Syntax.hs
@@ -2,3 +2,15 @@ module Inductive.Syntax
   (
   ) where
 
+data TypeConstructor = TypeConstructor
+  { name :: String
+  , args :: [Type]
+  , tp   :: Type
+  }
+
+data Type = IntType  Int
+          | BoolType Bool
+          | ProdType [Type]
+          | SumType  [Type]
+          | NamedADT
+          deriving (Show, Eq)
